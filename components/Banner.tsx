e318f68827798b53eb7f2f8eb8647202cd59d7d2
MubashirMian <138796628+MubashirMian@users.noreply.github.com>
2023-12-05 00:25:13 -0800

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -15,7 +15,7 @@ const Banner = () => {
       </div>
       {/* Description */}
       <div className="w-3/4 flex flex-col items-center md:items-start    gap-2">
-        <h1 className="text-5xl font-bold text-white">John Doe</h1>
+        <h1 className="text-5xl font-bold text-white">Mian A. M</h1>
         <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
           Full Stack Developer
         </h3>
