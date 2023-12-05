501145fce4d467248651ff447412272efe080db3
MubashirMian <138796628+MubashirMian@users.noreply.github.com>
2023-12-05 00:02:28 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Experience from '@/components/Experience'
 import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
-import Footer from '@/components/footer'
+import Footer from '@/components/Footer'
 export default function Home() {
   return (
     <main className='w-full bg-gray-950 text-gray-300 px-4'>
